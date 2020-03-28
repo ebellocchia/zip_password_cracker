60d567660fcb6e95a0fa9207f38f925ef82ff6b0
Emanuele <ebellocchia@gmail.com>
2020-03-28 19:41:48 +0100

    Assembly version data updated

--- VS2017_Project/ZipPasswordCracker/Properties/AssemblyInfo.cs
+++ VS2017_Project/ZipPasswordCracker/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ZipPasswordCracker")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Multithread brute force zip password cracker")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
+[assembly: AssemblyProduct("Zip Password Cracker")]
 [assembly: AssemblyCopyright("Copyright ©  2020 Emanuele Bellocchia")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
